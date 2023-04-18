9dd7912edd2179f08b9e705231025170261a46b5
HectorMG <hector980715@gmail.com>
2023-04-17 20:19:31 -0500

    Se crean modulos para auth y pages y se crea sistema de rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgessComponent } from './pages/progess/progess.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { PagesRoutingModule } from './pages/page-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard' , component: DashboardComponent},
-      { path: 'progress', component: ProgessComponent},
-      { path: 'grafica1', component: Grafica1Component},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NotpagefoundComponent},
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
