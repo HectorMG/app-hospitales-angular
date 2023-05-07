e844a876296f590cc885ae8de49b4ba947b7e0a9
HectorMG <hector980715@gmail.com>
2023-05-07 11:14:01 -0500

    Se implementa login de la app

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +11,43 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router){
+
+  public formularioEnviado = false;
+
+  public loginForm = this.formBuilder.group({
+    email: ['',[ Validators.required, Validators.email]],
+    password: ['',Validators.required]
+  });
+
+  constructor(private router: Router, private formBuilder: FormBuilder, private usuariosService: UsuarioService ){
 
   }
 
+  campoValido(campo:string){
+    if (this.loginForm.get(campo).invalid  && this.formularioEnviado) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
   login(){
-    this.router.navigateByUrl('/');
+    console.log(this.loginForm.value);
+    this.formularioEnviado = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.usuariosService.login(this.loginForm.getRawValue()).subscribe(
+      data => {
+        console.log(data);
+        this.router.navigateByUrl('/');
+      },
+      error=>{
+        Swal.fire('Error', error.error.msg,'error');
+      }
+    );
+
   }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { registerForm } from '../interfaces/register-form.interface';
 import { environment } from 'src/environments/environment.development';
+import { loginForm } from '../interfaces/login-form.interface';
 const base_url = environment.base_url;
 
 @Injectable({
@@ -12,8 +13,11 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
-
   crearUsuario( data: registerForm ){
     return this.http.post(`${base_url}/usuarios`,data);
+  }
+
+  login(data: loginForm){
+    return this.http.post(`${base_url}/auth/login`,data);
   }
 }
